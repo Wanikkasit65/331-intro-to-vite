39d6ac0b179220a9051c1a0a3b0ef37383c5867e
unknown <wanikkasit_n@cmu.ac.th>
2025-07-04 10:53:57 +0700

    i pause i am 8.5 now

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue'
 </script>
 
 <template>
+  <div class="home">
+    <EventCard />
+  </div>
   <main>
     <TheWelcome />
   </main>
